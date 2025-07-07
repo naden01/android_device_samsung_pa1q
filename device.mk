958d48949e9d6d6943ecf5ec12de9337a4d13894
nazephyrus <126683488+naden01@users.noreply.github.com>
2025-07-07 05:08:41 -0700

    a/b

--- device.mk
+++ device.mk
@@ -1,5 +1,22 @@
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+AB_OTA_UPDATER := true
+
+AB_OTA_PARTITIONS += \
+    dtbo \
+    boot \
+    vendor_dlkm \
+    init_boot \
+    system \
+    product \
+    system_ext \
+    vbmeta \
+    system_dlkm \
+    vendor \
+    vbmeta_system \
+    odm \
+    vendor_boot
+BOARD_USES_RECOVERY_AS_BOOT := true
 
 # fastbootd
 PRODUCT_PACKAGES += \
